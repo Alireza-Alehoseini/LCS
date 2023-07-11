8be14fa5b3fdd778bea895d9f594e56f20d104ff
alireza <103933753+Alireza-Alehoseini@users.noreply.github.com>
2023-07-11 13:30:12 -0700

    Add files via upload

---
+++ dna_matcher.py
@@ -0,0 +1,70 @@
+class DnaMatcher:
+    def __init__(self, body_dna, parent_dna):
+        self.body_dna = body_dna
+        self.parent_dna = parent_dna
+
+    def lcs_length(self):
+        body_length = len(self.body_dna)
+        parent_length = len(self.parent_dna)
+
+        c = [[0] * (parent_length + 1) for _ in range(body_length + 1)]
+        b = [[""] * (parent_length + 1) for _ in range(body_length + 1)]
+            
+        """
+        for i in range(1, body_length + 1):           
+            c[i][0] = 0
+    
+        for j in range(parent_length + 1):
+            c[0][j] = 0
+        """
+
+        for i in range(1, body_length + 1):
+            for j in range(1, parent_length + 1):
+                if self.body_dna[i - 1] == self.parent_dna[j - 1]:
+                    c[i][j] = c[i - 1][j - 1] + 1
+                    b[i][j] = "↖"
+                elif c[i - 1][j] >= c[i][j - 1]:
+                    c[i][j] = c[i - 1][j]
+                    b[i][j] = "↑"
+                else:
+                    c[i][j] = c[i][j - 1]
+                    b[i][j] = "←"
+
+        return self.print_lcs(b, body_length, parent_length)
+
+    def print_lcs(self, b, i, j):
+        result = ""
+        if i == 0 or j == 0:
+            return result
+
+        if b[i][j] == "↖":
+            result += self.print_lcs(b, i - 1, j - 1)
+            result += self.body_dna[i - 1]
+        elif b[i][j] == "↑":
+            result += self.print_lcs(b, i - 1, j)
+        else:
+            result += self.print_lcs(b, i, j - 1)
+
+        return result
+
+
+class DnaFileHandler:
+    def __init__(self, body_filename, parents_filename, result_filename):
+        self.body_filename = body_filename
+        self.parents_filename = parents_filename
+        self.result_filename = result_filename
+
+    def read_body_dna(self):
+        with open(self.body_filename, 'r') as f:
+            return f.readline().strip()
+
+    def read_parents_dna(self):
+        parents_dna = []
+        with open(self.parents_filename, 'r') as f:
+            for line in f:
+                parents_dna.append(line.strip())
+        return parents_dna
+
+    def write_results(self, results):
+        with open(self.result_filename, 'w') as f:
+            f.write('\n'.join(results))
